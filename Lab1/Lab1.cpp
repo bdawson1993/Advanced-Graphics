6df388c68241de4021d6238e0c14b99c9a466cb8
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2019-09-29 16:23:03 +0100

    latest changes

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -26,8 +26,8 @@ int main( void )
 
 	};
 
-	Shape* triangle = new Shape(g_vertex_buffer_data, sizeof(g_vertex_buffer_data), graphics->GetProgramID());
-	Shape* triangle2 = new Shape(g_vertex_buffer_data2, sizeof(g_vertex_buffer_data2), graphics->GetProgramID());
+	Shape* triangle = new Shape(g_vertex_buffer_data, sizeof(g_vertex_buffer_data), graphics->GetProgramID(), &graphics->GetView(), &graphics->GetProjection());
+	Shape* triangle2 = new Shape(g_vertex_buffer_data2, sizeof(g_vertex_buffer_data2), graphics->GetProgramID(), &graphics->GetView(), &graphics->GetProjection());
 
 	//graphics->AddShapeBuffer(g_vertex_buffer_data, sizeof(g_vertex_buffer_data));
 	graphics->AddShapeToScene(*triangle);
