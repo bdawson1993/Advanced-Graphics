5931f2e506e9ad51dd465640f651f3fa51943ff7
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2019-10-02 20:12:01 +0100

    Create Cube class

---
+++ Lab1/Cube.cpp
@@ -0,0 +1,53 @@
+#include "Cube.h"
+
+Cube::Cube(GLuint programId, WindowCamera& cam) : Shape(cam)
+{
+	shaderID = programId;
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f
+	};
+
+	FillBuffer("vertexbuffer", g_vertex_buffer_data, sizeof(g_vertex_buffer_data));
+	SetupShaderAttribute("vertexbuffer", 3, GL_FLOAT);
+	size = 12 * 3;
+	SetColor(1, 1, 1);
+
+
+	GetUniform("MVP");
+	GetUniform("inColor");
+}
---
+++ Lab1/Cube.h
@@ -0,0 +1,13 @@
+#pragma once 
+#include "Shape.h"
+
+
+class Cube : public Shape
+{
+public:
+	Cube(GLuint programId, WindowCamera& cam);
+	~Cube();
+
+private:
+
+};
